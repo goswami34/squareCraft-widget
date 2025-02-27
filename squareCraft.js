9b3a5fa2a211e52707ea8b090f1977847c052df1
goswami34 <amit@webefo.com>
2025-02-27 16:13:49 +0600

    update

--- squareCraft.js
+++ squareCraft.js
@@ -685,15 +685,7 @@
 fontfamilies();
 
 
-document.getElementById("squareCraftFontWeight").addEventListener("change", function () {
-    if (selectedElement) {
-        let selectedWeight = this.value;
-        let css = { "font-weight": selectedWeight };
 
-        applyStylesToElement(selectedElement.id, css);
-        saveModifications(selectedElement.id, css);
-    }
-});
   
  
   
