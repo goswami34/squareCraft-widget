94166ac53d958f93f976e04d30136b52f6cab11b
goswami34 <amit@webefo.com>
2025-03-02 13:43:37 +0600

    dgsg

--- squareCraft.js
+++ squareCraft.js
@@ -256,7 +256,8 @@
 
               
 
-               <label>Font Weight:</label>
+               <div>
+                <label>Font Weight:</label>
                 <select id="squareCraftFontWeight" style="width: 100%; padding: 6px; background: #2c2c2c; color: white; border: 1px solid #585858; border-radius: 6px; margin:4px;">
                     <option value="100">Thin (100)</option>
                     <option value="200">Extra Light (200)</option>
@@ -268,6 +269,7 @@
                     <option value="800">Extra Bold (800)</option>
                     <option value="900">Black (900)</option>
                 </select>
+               </div>
 
 
               <div class="squareCraft-mt-2  squareCraft-grid squareCraft-px-2 squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 ">
