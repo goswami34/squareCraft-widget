ebf67cb0a936e2f584a04031ad660ad602dfa28f
goswami34 <amit@webefo.com>
2025-03-03 12:14:47 +0600

    sgsgs

--- squareCraft.js
+++ squareCraft.js
@@ -33,7 +33,7 @@
     const link = document.createElement("link");
     link.rel = "stylesheet";  
     link.type = "text/css";
-    link.href = "https://fatin-webefo.github.io/squareCraft-Plugin/src/styles/parent.css";
+    link.href = "https://fatin-webefo.github.io/squareCraft-plugin/src/styles/parent.css";
     document.head.appendChild(link);
   
     const fontSizes = [8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36 , 37, 38, 39, 40, 41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, 52, 53, 54, 55, 56, 57, 58, 59, 60];
