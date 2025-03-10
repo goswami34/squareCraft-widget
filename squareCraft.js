96d86a76fb962c77e3226e733ee37c790d2be7ac
goswami34 <amit@webefo.com>
2025-03-10 13:01:41 +0600

    fgsgs

--- squareCraft.js
+++ squareCraft.js
@@ -497,7 +497,10 @@
 
               <div class="squareCraft-mt-2  squareCraft-grid squareCraft-px-2 squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 ">
               
-        
+                
+
+                 
+              </div>
 
               <div class="squareCraft-mt-2 squareCraft-grid squareCraft-px-2 squareCraft-w-full squareCraft-grid-cols-12 squareCraft-gap-2 ">
                  <div class="squareCraft-flex squareCraft-col-span-5 squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center ">
