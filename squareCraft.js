3079a039860532b9b376769d81f2d77e0816c484
goswami34 <amit@webefo.com>
2025-03-19 14:51:34 +0600

    dhdhd

--- squareCraft.js
+++ squareCraft.js
@@ -860,14 +860,11 @@ function cleanupDuplicateSpans(elementId) {
                   <div class="squareCraft-flex squareCraft-col-span-6 squareCraft-justify-between squareCraft-border squareCraft-border-solid squareCraft-border-585858 squareCraft-rounded-6px squareCraft-items-center ">
                       <div
                         class="squareCraft-flex squareCraft-px-2 squareCraft-items-center squareCraft-justify-between squareCraft-w-full ">
-                        <p class="squareCraft-font-bold squareCraft-universal squareCraft-text-sm squareCraft-cursor-pointer elements-font-style" data-style="bold">B</p>
-                        <div class="squareCraft-v-line"></div>
-                        <p  class="squareCraft-font-italic squareCraft-universal  squareCraft-text-sm squareCraft-cursor-pointer squareCraft-text-center squareCraft-mx-auto elements-font-style" data-style="italic">I</p>
-                        <div class="squareCraft-v-line"></div>
-                      <p class="squareCraft-font-underline squareCraft-universal squareCraft-text-sm squareCraft-cursor-pointer squareCraft-text-center squareCraft-mx-auto elements-font-style" data-style="underline">U</p>
+                      
+                      
+                        <p class="squareCraft-font-underline squareCraft-universal squareCraft-text-sm squareCraft-cursor-pointer squareCraft-text-center squareCraft-mx-auto elements-font-style" data-style="underline">U</p>
                         <div class="squareCraft-v-line"></div> 
-                        <p  class="squareCraft-font-underline squareCraft-universal squareCraft-text-sm squareCraft-cursor-pointer squareCraft-text-center squareCraft-mx-auto elements-font-style" data-style="dotted">abc</p>
-                          <div class="squareCraft-v-line"></div> 
+                       
                         <img class=" squareCraft-rounded-6px squareCraft-rotate-180 squareCraft-px-1_5 squsareCraft-font-style squareCraft-cursor-pointer" width="12px"
                       src="https://fatin-webefo.github.io/squareCraft-plugin/public/dot.svg" alt="">
                       </div>
