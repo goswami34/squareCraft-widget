e98545dc41de59acc979856957971f291a402403
goswami34 <amit@webefo.com>
2025-04-09 18:01:07 +0600

    afafaf

--- src/components/Typo/typoBoldSelect.js
+++ src/components/Typo/typoBoldSelect.js
@@ -3,9 +3,9 @@ export function typoBoldSelect(fontSizes) {
         <div
    class="sc-mt-2 sc-relative sc-grid  sc-grid-cols-12 sc-gap-2 ">
 
-   <div id="scFontSelect"
+   <div id="sc-font-family"
       class="sc-flex sc-bg-494949 sc-h-9 sc-col-span-8 sc-rounded-6px sc-justify-between sc-border sc-border-solid sc-border-585858 sc-items-center">
-      <div id="sc-font-family" 
+      <div  
          class=" sc-text-sm sc-poppins sc-font-light"
          style="background: transparent; color: white; border: none; outline: none; appearance: none; cursor: pointer; padding: 0 8px;"> 
       </div>
