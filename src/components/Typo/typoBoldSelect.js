11c948b932f9e956a189a6f32c8c087090e7dede
goswami34 <amit@webefo.com>
2025-04-12 15:47:11 +0600

    gsgfsgs

--- src/components/Typo/typoBoldSelect.js
+++ src/components/Typo/typoBoldSelect.js
@@ -94,7 +94,7 @@ export function typoBoldSelect(fontSizes) {
     <div
      class="sc-flex sc-col-span-5 sc-justify-between  sc-items-center ">
     <div
-        class="sc-flex sc-items-center" id="squareCraftTextTransform>
+        class="sc-flex sc-items-center" id="squareCraftTextTransform">
         
         <div id="scTextTransformUppercase" data-text-transform="uppercase" class="sc-pt-1 sc-pb-1 sc-px-1_5 sc-inActiveTab-border sc-cursor-pointer sc-rounded-l">
 
