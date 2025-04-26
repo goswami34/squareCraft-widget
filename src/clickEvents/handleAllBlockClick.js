bcd25e107de0b0345b73eceb26a5c8f9dd24d64e
goswami34 <amit@webefo.com>
2025-04-26 11:32:33 +0600

    afdafaf

--- src/clickEvents/handleAllBlockClick.js
+++ src/clickEvents/handleAllBlockClick.js
@@ -230,5 +230,26 @@ export function handleAllBlockClick(event, context) {
     });
   });
 
+  visibleParts.forEach((partId) => {
+    const typeId = partId.replace("Part", "");
+    const tab = document.getElementById(typeId);
+    if (!tab) return;
+
+    tab.onclick = () => {
+      const clickedTag = typeId.startsWith("heading")
+        ? `h${typeId.replace("heading", "")}`
+        : typeId;
+      setSelectedSingleTextType(clickedTag);
+      console.log("✅ Now selected text type:", clickedTag);
+
+      const textAlignInput = document.getElementById(
+        `scTextAlignInput-${typeId}`
+      );
+      if (textAlignInput) {
+        textAlignInput.focus();
+      }
+    };
+  });
+
   //all section font size code end here
 }
