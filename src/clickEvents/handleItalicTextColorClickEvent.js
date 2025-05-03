99c4c92a790092cb542d78b032dd8f0d30654135
goswami34 <amit@webefo.com>
2025-05-03 17:09:42 +0600

    vbcdfgxcg

--- src/clickEvents/handleItalicTextColorClickEvent.js
+++ src/clickEvents/handleItalicTextColorClickEvent.js
@@ -1,7 +1,7 @@
 let colorPalette = null;
 let colorPickerContext = null;
 
-export function handleTextColorClick(
+export function handleItalicTextColorClickEvent(
   event,
   lastClickedElement,
   applyStylesToElement,
