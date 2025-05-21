678143446e0d8ec77cbd5a225905d7b095c1f5c8
goswami34 <amit@webefo.com>
2025-05-21 11:23:42 +0600

    adfafafa

--- src/utils/Image/initImageBorderControls.js
+++ src/utils/Image/initImageBorderControls.js
@@ -333,28 +333,10 @@ export function initImageBorderControls(selectedElement, context = {}) {
     }`;
 
     // ✅ Save imageTag styles to DB along with main styles
-    // mergeAndSaveImageStyles(
-    //   blockId,
-    //   {
-    //     ...cssProps,
-    //     imageTag: {
-    //       selector: imageTagSelector,
-    //       styles: {
-    //         "box-sizing": "border-box",
-    //         "object-fit": "cover",
-    //       },
-    //     },
-    //   },
-    //   saveModificationsforImage
-    // );
-
     mergeAndSaveImageStyles(
       blockId,
       {
-        image: {
-          selector: `#${blockId} div.sqs-image-content`,
-          styles: cssProps,
-        },
+        ...cssProps,
         imageTag: {
           selector: imageTagSelector,
           styles: {
