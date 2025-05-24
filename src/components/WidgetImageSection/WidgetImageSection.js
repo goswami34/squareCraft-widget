9dd042ebde7af76398b02b60c93c6ec496165479
goswami34 <amit@webefo.com>
2025-05-24 14:57:26 +0600

    fgsgsgsgsg

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -557,8 +557,8 @@ export function WidgetImageSection(id) {
 
 
 
-                     <div id="bgColorSection" class=" ">
-                  <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto">background Color
+               <div id="bgColorSection" class=" ">
+                  <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Color
                   </p>
                   <div class="sc-col-span-5 sc-mt-2 sc-z-99999 sc-relative ">
                      <div class="sc-flex sc-w-30 sc-justify-between sc-items-center sc-px-2 sc-bg-3f3f3f sc-inActiveTab-border sc-rounded-6px sc-py-0_5">
@@ -602,6 +602,41 @@ export function WidgetImageSection(id) {
                      </div>
                   </div>
                </div>
+
+
+
+
+                  <div class="sc-mt-4">
+                        <div class="  sc-flex sc-items-center sc-justify-between">
+                           <div class="sc-flex sc-gap-2 sc-items-center">
+                              <p class="sc-roboto sc-font-thin sc-universal  sc-font-size-12 sc-font-thin sc-text-gray-300">
+                                 Blur
+                              </p>
+                              <div
+                                 class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-1 sc-p-1_5  sc-bg sc-bg-454545">
+                                 <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/reset.svg" width="10"
+                                    alt="reset">
+                              </div>
+                           </div>
+                           <div
+                              class="sc-flex sc-cursor-pointer sc-items-center sc-rounded-15px sc-gap-2 sc-px-2 sc-py-0_5 sc-bg sc-bg-454545">
+                              <p class="sc-font-thin sc-roboto sc-universal sc-font-size-11">0px</p>
+                              <div class="sc-flex sc-flex-col sc-items-center sc-gap-1">
+                                 <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" alt=""
+                                    width="9">
+                                 <img src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg" width="9"
+                                    class="sc-rotate-180" alt="">
+                              </div>
+                           </div>
+                        </div>
+                        <div class="sc-rounded-15px sc-mt-3 sc-relative sc-w-full sc-h-2 sc-bg-F6F6F6">
+                           <div
+                              class="sc-absolute sc-bg-color-EF7C2F sc-w-3 sc-h-3 sc-rounded-full sc-cursor-pointer sc-top-half">
+                           </div>
+                        </div>
+                     </div>
+
+
 
 
 
