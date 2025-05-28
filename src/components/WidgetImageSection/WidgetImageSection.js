965c11e8780cb59631907ebccaa95fe4ecf6a121
goswami34 <amit@webefo.com>
2025-05-28 12:06:04 +0600

    sfgsgsg

--- src/components/WidgetImageSection/WidgetImageSection.js
+++ src/components/WidgetImageSection/WidgetImageSection.js
@@ -355,55 +355,16 @@ export function WidgetImageSection(id) {
 
                   <div class="sc-mt-4 sc-gap-2 sc-flex">
 
-
-                     <div id="bgColorSection" class=" ">
-                  <p class="sc-font-size-11 sc-font-thin sc-mt-4 sc-universal sc-text-gray-300 sc-roboto"> Color
-                  </p>
-                  <div class="sc-col-span-5 sc-mt-2 sc-z-99999 sc-relative ">
-                     <div class="sc-flex sc-w-30 sc-justify-between sc-items-center sc-px-2 sc-bg-3f3f3f sc-inActiveTab-border sc-rounded-6px sc-py-0_5">
-                     <p id="buttonFontColorCode" class="sc-font-size-12 sc-roboto sc-universal">Select</p>
-                     <div id="buttonFontColorPalate" class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
-                     <div id="button-font-color-palette" class="...sc-hidden..."></div>
-
-                     </div>
-                     <div id="button-font-color-palette"
-                     class="sc-absolute sc-z-99999 sc-border sc-hidden sc-border-solid sc-border-EF7C2F sc-top-12 sc-bg-3f3f3f sc-left-0 sc-p-1_5 sc-rounded-6px">
-                     <div class="sc-button-fontcolor-arrow"></div>
-                     <div class="sc-flex sc-items-center sc-justify-between">
-                        <div id="button-border-colors" class="sc-flex sc-relative sc-items-center sc-gap-1"></div>
-                        <div class="sc-rounded-15px sc-px-1_5 sc-py-0_5 sc-bg-454545 sc-flex sc-gap-1">
-                           <p class="sc-universal sc-font-size-11 sc-roboto">RGB</p>
-                           <img id="buttonParagraph1Arrow" width="10"
-                           src="https://fatin-webefo.github.io/squareCraft-plugin/public/arrow.svg"
-                           class="sc-rotate-180" alt="">
+                  
+                     <div class="">
+                        <p class="sc-roboto sc-font-thin sc-universal  sc-text-sm sc-font-thin sc-text-gray-300">
+                           Color
+                        </p>
+                        <div class="sc-py-0_5 sc-gap-5 sc-mt-2 sc-bg-3f3f3f sc-inActiveTab-border sc-flex sc-justify-between sc-items-center sc-px-1_5 sc-rounded-6px">
+                           <p id="overlay-button-color-code" class="sc-text-xs sc-roboto sc-font-light sc-universal">#363544</p>
+                           <div id="overlay-button-border-colors color-selection-fielda" class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
                         </div>
                      </div>
-                     <div class="sc-h-1px sc-mt-2 sc-bg-color-gray"></div>
-                     <div class="sc-flex color-h-selection sc-mt-2 sc-items-center sc-gap-2">
-                        <div id="button-color-selection-field" class="sc-relative">
-                           <div id="button-color-selection-bar"
-                           class="sc-w-2 sc-h-2 sc-absolute sc-cursor-pointer sc-rounded-full sc-border sc-border-solid sc-border-white">
-                           </div>
-                        </div>
-                        <div id="button-color-transparency-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-                           <div id="button-color-transparency-bar"
-                           class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2">
-                           </div>
-                        </div>
-                        <div id="button-all-color-selection-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-                           <div id="button-all-color-selection-bar"
-                           class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2">
-                           </div>
-                        </div>
-                     </div>
-                     <div class="sc-flex sc-justify-between sc-mt-2 sc-px-2 sc-py-0_5 sc-rounded-6px sc-bg-454545">
-                        <p id="button-color-code" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">Select</p>
-                        <p id="button-color-transparency-count" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">100%</p>
-                     </div>
-                     </div>
-                  </div>
-               </div>
-
 
 
 
