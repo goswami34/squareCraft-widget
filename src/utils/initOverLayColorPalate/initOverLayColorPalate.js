237b9f6428c7d763ea68f891e014757b1e3207f5
goswami34 <amit@webefo.com>
2025-06-01 14:23:52 +0600

    adfadfa

--- src/utils/initOverLayColorPalate/initOverLayColorPalate.js
+++ src/utils/initOverLayColorPalate/initOverLayColorPalate.js
@@ -651,9 +651,7 @@ export function initOverLayColorPalate(
         height: 100%;
         z-index: 1;
       }
-      #${blockId} .sqs-image-content > :nth-child(-n+2) {
-        position: relative;
-      }
+      
     `;
   }
 
