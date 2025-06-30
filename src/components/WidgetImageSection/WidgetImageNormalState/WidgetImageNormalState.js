43b053ec44ce2f2ca86031289590deae7ba74ef1
goswami34 <amit@webefo.com>
2025-06-30 16:41:28 +0600

    syhsyhsetwrtwt

--- src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
+++ src/components/WidgetImageSection/WidgetImageNormalState/WidgetImageNormalState.js
@@ -278,7 +278,10 @@ export function WidgetImageNormalState() {
          </div>
          <div class="sc-flex sc-mt-2 sc-items-center">
             <div class="sc-bg-3f3f3f sc-py-1 sc-px-2 sc-rounded-l">
-               <p class="sc-universal sc-roboto sc-text-sm ">Yes</p>
+               <select id="overlayVisibleDropdown" class="sc-universal sc-roboto sc-text-sm sc-bg-3f3f3f sc-text-white sc-border-none focus\:outline-none">
+                  <option value="yes" selected>Yes</option>
+                  <option value="no">No</option>
+               </select>
             </div>
             <div class="sc-bg-454545 sc-flex sc-items-center sc-justify-center sc-px-2_5 sc-py-13px"><span  class="sc-arrow-placeholder sc-rotate-180"></span></div>
          </div>
@@ -508,39 +511,41 @@ export function WidgetImageNormalState() {
       <div class="sc-col-span-5 sc-mt-2 sc-z-99999 sc-relative ">
          <div class="sc-flex sc-w-30 sc-justify-between sc-items-center sc-px-1 sc-bg-3f3f3f sc-inActiveTab-border sc-rounded-4px sc-py-4px">
          <p  class="sc-font-size-12 sc-roboto sc-universal">Select</p>
-         <div  class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
+         <div id="overLayFontColorPalate" class="sc-square-6 sc-border-colors sc-cursor-pointer"></div>
          </div>
-         <div 
+         <div id="overlay-color-palette"
          class="sc-absolute sc-z-99999 sc-border sc-hidden sc-border-solid sc-border-EF7C2F sc-top-12 sc-bg-3f3f3f sc-left-0 sc-p-1_5 sc-rounded-4px">
          <div class="sc-image-fontcolor-arrow"></div>
          <div class="sc-flex sc-items-center sc-justify-between">
-            <div  class="sc-flex sc-relative sc-items-center sc-gap-1"></div>
+            <div id="overlay-border-colors" class="sc-flex sc-relative sc-items-center sc-gap-1"></div>
             <div class="sc-rounded-15px sc-px-1_5 sc-py-0_5 sc-bg-454545 sc-flex sc-gap-1">
                <p class="sc-universal sc-font-size-11 sc-roboto">RGB</p>
                      <span  class="sc-arrow-placeholder sc-rotate-180"></span>
             </div>
          </div>
+
          <div class="sc-h-1px sc-mt-2 sc-bg-color-gray"></div>
+
          <div class="sc-flex color-h-selection sc-mt-2 sc-items-center sc-gap-2">
-            <div id="image-color-selection-field" class="sc-relative">
-               <div id="image-color-selection-bar"
+            <div id="overlay-color-selection-field" class="sc-relative">
+               <div id="overlay-color-selection-bar"
                class="sc-w-2 sc-h-2 sc-absolute sc-cursor-pointer sc-rounded-full sc-border sc-border-solid sc-border-white">
                </div>
             </div>
-            <div id="image-color-transparency-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-               <div id="image-color-transparency-bar"
+            <div id="overlay-image-color-transparency-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
+               <div id="overlay-image-color-transparency-bar"
                class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2">
                </div>
             </div>
-            <div id="image-all-color-selection-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
-               <div id="image-all-color-selection-bar"
+            <div id="overlay-image-all-color-selection-field" class="sc-h-full sc-w-3 sc-relative sc-rounded-15px">
+               <div id="overlay-image-all-color-selection-bar"
                class="sc-absolute sc-w-5 sc-left-half sc-shadow-sm sc-rounded-15px sc-cursor-grabbing sc-h-2 sc-bg-color-f2f2f2">
                </div>
             </div>
          </div>
          <div class="sc-flex sc-justify-between sc-mt-2 sc-px-2 sc-py-0_5 sc-rounded-4px sc-bg-454545">
-            <p id="image-color-code" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">Select</p>
-            <p id="image-color-transparency-count" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">100%</p>
+            <p id="overlay-image-color-code" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">Select</p>
+            <p id="overlay-image-color-transparency-count" class="sc-font-size-12 sc-roboto sc-font-light sc-universal">100%</p>
          </div>
          </div>
       </div>
